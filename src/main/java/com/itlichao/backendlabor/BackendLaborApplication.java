6cf0c1b2e97c371c0c1d994cff95117434d23036
itcong <760042297@qq.com>
2026-03-07 09:47:47 +0800

    初始化

---
+++ src/main/java/com/itlichao/backendlabor/BackendLaborApplication.java
@@ -0,0 +1,13 @@
+package com.itlichao.backendlabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLaborApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendLaborApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itlichao/backendlabor/BackendLaborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlichao.backendlabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLaborApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
